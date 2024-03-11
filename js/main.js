8cdf8ff814f585b76877154ac6d481a4a1edd337
Anton Surygin <surygin@live.com>
2024-03-11 14:57:57 +0300

    дописал скрипты для z-index

--- js/main.js
+++ js/main.js
@@ -1,13 +1,24 @@
 const mainFormClose = document.getElementById("mainFormClose");
 const mainForm = document.getElementById("mainForm");
 const btnHelp = document.getElementById("btnHelp");
+const notbgBody = document.getElementsByClassName("notbg__body");
+
+function changeZindex(zIndex){
+    for (const section of notbgBody) {
+        section.style.zIndex = zIndex;
+    }
+}
 
 mainFormClose.addEventListener('click', ()=>{
-    mainForm.classList.remove("fade__in")
+    mainForm.classList.remove("fade__in");
+    btnHelp.style.zIndex = "2";
+    changeZindex(1);
 });
 
 btnHelp.addEventListener('click', ()=>{
-    mainForm.classList.add("fade__in")
+    mainForm.classList.add("fade__in");
+    btnHelp.style.zIndex = "0";
+    changeZindex(0);
 })
 
 document.addEventListener('keydown', event=>{
